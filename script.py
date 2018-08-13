413a8f398a1d9ac4149f5fc501b463ebb972c6bd
gurmukh <gurmukh.singh511@gmail.com>
2018-08-13 12:24:58 +0530

    added 1d and 2d cnn with predict function

--- script.py
+++ script.py
@@ -1,15 +1,11 @@
-
+import gc
 import numpy as np
-import keras
-import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPool2D, Dropout, Dense, Flatten, Conv1D, MaxPool1D, Lambda
 from keras.layers import *
-import pandas as pd
-
-data = np.load('11785-hw2pt2/train-features.npy', encoding='bytes')
-labels = np.load('11785-hw2pt2/train-labels.npy', encoding='bytes')
-print('data and labels loaded')
+import time
+from keras import optimizers
+import tensorflow as tf
 
 def preprocess_single_label(label):  # input single label which is an array containing phoneme for each frame
     one_hot = []
@@ -22,42 +18,12 @@ def preprocess_single_label(label):  # input single label which is an array cont
     return one_hot
 
 
-
-#
-# dev_data = np.load('11785-hw2pt2/dev-features.npy', encoding='bytes')
-# dev_labels = np.load('11785-hw2pt2/dev-labels.npy', encoding='bytes')
-
-# dev_x_train = []
-# dev_label = []
-# for i in range(dev_data.shape[0]):
-#     x = preprocess_single_x(dev_data[i])
-#     y = preprocess_single_label(dev_labels[i])
-#     x_train, y_train = final_data(x, y)
-#     #     print(x_train.shape)
-#
-#     dev_x_train.append(x_train)
-#     dev_label.append(y_train)
-#
-# dev_x_train = np.vstack(dev_x_train)
-# # print(final_x_train.shape)
-# dev_label = np.vstack(dev_label)
-
-
-data_1 = np.array([data[i] for i in range(data.shape[0]) if data[i][0].shape[0] <= 1350])
-label_1 = np.array([labels[i] for i in range(data.shape[0]) if data[i][0].shape[0] <= 1350])
-
 def variable_len_pool(x, data):
     frame_list = []
     time_info = data[3][1]
     for i in range(len(time_info) - 1):  ### need to take into account that time info dont have the last frame number
         frame_list.append(np.mean(x[:, time_info[i]:time_info[i + 1]]))
     return np.asarray(frame_list)
-
-#
-# phoneme_list = ["+BREATH+", "+COUGH+", "+NOISE+", "+SMACK+", "+UH+", "+UM+", "AA", "AE", "AH", "AO", "AW",
-#                 "AY", "B", "CH", "D", "DH", "EH", "ER", "EY", "F", "G", "HH", "IH", "IY", "JH", "K", "L",
-#                 "M", "N", "NG", "OW", "OY", "P", "R", "S", "SH", "SIL", "T", "TH", "UH", "UW", "V", "W", "Y",
-#                 "Z", "ZH"]
 
 def pad_cnn(x, max_len=1350):
     diff = max_len - x.shape[0]
@@ -66,7 +32,7 @@ def pad_cnn(x, max_len=1350):
     if diff > 0:
         zeros = np.zeros((diff, 40))  # 40 mel spec
         return np.concatenate((x, zeros))
-
+# 
 
 def mask(label, time_boundary, max_len=1350):
     one_hot_label = preprocess_single_label(label)
@@ -88,7 +54,7 @@ def time_boundary_pair(data):
             time_pair.append((t[i], t[i + 1]))
     return time_pair
 
-
+# 
 def process_data(data, idx_1, idx_2, labels):
     time_list = []
     for i in range(data.shape[0]):
@@ -109,46 +75,119 @@ def process_data(data, idx_1, idx_2, labels):
 # defining global variables for slicing output of cnn
 import time
 import keras.backend as K
-a = K.variable(0, dtype=np.int32)
-b = K.variable(8, dtype=np.int32)
+a = K.variable(-2, dtype=np.int32)  # type should be int because we will use a and b for slicing tensors
+b = K.variable(-1, dtype=np.int32)   # using -ve values just to be sure that these are changing durinng training .if not then it will throw error
 
-def crop(x):
-    print(x[:, a:b, :, :].shape)
-    return x[:, a:b, :, :]
+# def crop(x):    #use this for 2d cnn
+#     import tensorflow as tf
+#     print(x[:, a:b, :, :].shape)
+#     return x[:, a:b, :, :]
+
+def crop(x):  #use this for 1d cnn
+    import tensorflow as tf
+
+#     print(tf.reshape(tf.reduce_mean(x[:,a:b,:],axis=1),(1,64)))
+#     print(a,b)1
+    return tf.reduce_mean(x[:,a:b,:],axis=1)
+    
+
+def two_d_cnn():
+	model = Sequential()
+	model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='same', input_shape=(1350, 40, 1), activation='relu'))
+	model.add(Conv2D(filters=64, kernel_size=(5, 5), padding='same', activation='relu'))
+	model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
+	model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu'))
+
+	# model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
+	model.add(Lambda(crop, output_shape=(None, 40, 256)))
+	model.add(GlobalAveragePooling2D())
+	model.add(Dense(46, activation='softmax'))
+	return model
+
+def one_d_cnn():
+	model=Sequential()
+	model.add(Conv1D(filters=32,kernel_size=11,padding='same',input_shape=(1350,40),activation='relu'))
+	model.add(Conv1D(filters=64,kernel_size=7,padding='same',activation='relu'))
+	model.add(Conv1D(filters=128,kernel_size=5,padding='same',activation='relu'))
+	model.add(Conv1D(filters=256,kernel_size=3,padding='same',activation='relu'))
+	model.add(Conv1D(filters=512,kernel_size=3,padding='same',activation='relu'))
+
+	model.add(Lambda(crop,output_shape=(512,)))
+	model.add(Dense(128,activation='relu'))
+	model.add(Dense(46,activation='softmax'))
+	return model
+
+def predict(dev_data_1,dev_labels_1):
+	all_pred=[]
+	for q in range(dev_data_1.shape[0]):
+	    if q%50==0:
+	        print("predicting {} th dev".format(q))
+	    pred_list=[]
+	    label_len=dev_data_1[q][1].shape[0]
+	    x_dev=pad_cnn(dev_data_1[q][0])
+	    x_=np.repeat(x_dev.reshape(1,1350,40),label_len,axis=0)   # for 1d cnn
+	    # x_=np.repeat(x_dev.reshape(1350,40,1),label_len,axis=0)  
+
+	    y_=preprocess_single_label(dev_labels_1[q])  # 57,46
+
+	    for i,j,p in zip(x_,y_,time_boundary_pair(dev_data_1[q])):
+	        K.set_value(a,p[0])
+	        K.set_value(b,p[1])
+	#         print(p)
+	        pred_list.append(model.predict(x=i.reshape(1,1350,40)))   # change shape to 1,1350,40 for 1d cnn
+	    all_pred.append(pred_list)
+	acc=[]
+	for i in range(len(all_pred)):
+	    p=np.argmax(np.vstack(all_pred[i]),axis=1)
+	    t=dev_labels_1[i]
+	    if p.shape == t.shape:
+	        acc.append(np.sum(np.equal(p,t))/np.equal(p,t).shape[0])
+    
+	print("accuracy is {}".format(np.array(acc).mean()))
+	return np.array(acc).mean()
 
 
-model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='same', input_shape=(1350, 40, 1), activation='relu'))
-model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-# model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(Lambda(crop, output_shape=(None, 40, 64)))
-model.add(GlobalAveragePooling2D())
-model.add(Dense(46, activation='softmax'))
+########## main starts ###################3
 
+model=one_d_cnn()
 model.summary()
-model.compile(optimizer='sgd', loss='categorical_crossentropy')
+sgd = optimizers.SGD(lr=0.001)
+model.compile(optimizer=sgd,loss='categorical_crossentropy')
 
 
+data = np.load('11785-hw2pt2/train-features.npy', encoding='bytes')
+labels = np.load('11785-hw2pt2/train-labels.npy', encoding='bytes')
+dev_data=np.load('11785-hw2pt2/dev-features.npy',encoding='bytes')
+dev_labels=np.load('11785-hw2pt2/dev-labels.npy',encoding='bytes')
 
-import gc
+dev_data_1=np.array([dev_data[i] for i in range(dev_data.shape[0]) if dev_data[i][0].shape[0]<=1350])
+dev_labels_1=np.array([dev_labels[i] for i in range(dev_data.shape[0]) if dev_data[i][0].shape[0]<=1350])
+data_1 = np.array([data[i] for i in range(data.shape[0]) if data[i][0].shape[0] <= 1350])
+label_1 = np.array([labels[i] for i in range(data.shape[0]) if data[i][0].shape[0] <= 1350])
 
+print('data and labels loaded')
 print('training started')
 ####  batched approach ##########
-####  batched approach ##########
-# for i in range(1):
+
 gc.collect()
-model.load_weights('2d_CNN_dot_weights_')
-for i in range(1,800):
+model.load_weights('1d_CNN_dot_weights_8')
+# file=open("train_log.txt","w")
+t=time.time()
+for i in range(9,150):
     print("CURRENTLY ON INDEX ",i)
     batch_gen=process_data(data_1,i,i+1,label_1)
     c=0
     q=0
+    batch_num=0
     for batch in batch_gen: #range(len(w))
     #     print(len(batch))
     #     print('q',q)
-    #     q+=len(batch)
+        q+=len(batch)
+        batch_num+=1
+        # if batch_num>20:
+        # 	break
         x=np.array([pad_cnn(instance[0][0]) for instance in batch])
-        x=x.reshape(x.shape[0],x.shape[1],x.shape[2],1)
+        # x=x.reshape(x.shape[0],x.shape[1],x.shape[2],1)   #uncomment for 2d cnn
         y=np.array([preprocess_single_label(instance[1])[i] for instance in batch])
         z=0
         for instance in batch:
@@ -162,94 +201,40 @@ for i in range(1,800):
         c+=1
 
         gc.collect()
-        if x.shape[0]<20:
+        if i<10 and x.shape[0]==13:
             break
-    # model.save('2d_CNN_dot_save_{}'.format(i))
-    model.save_weights('2d_CNN_dot_weights_{}'.format(i))
+        if i<20 and x.shape[0]==8:
+            break  
+        if i<30 and x.shape[0]==5:
+            break 
+        if i<40 and x.shape[0]==2:
+            break  
+                  
+        print("total time elapsed {} sec".format(time.time()-t))
+        print("trained on {} utterances".format(q))
+    with open("train_log_2d_cnn.txt","a") as file:
+        file.write("trained on index {},using {} utterances.total time elapsed : {} sec +\n".format(i,q,time.time()-t))    
 
-# In[ ]:
-#############################################################################################################333
+    if i%4==0:
+        accuracy=predict(dev_data_1[:150],dev_labels_1[:150])
 
-# saving previous work before making new changes
-####  batched approach ##########
-# for i in range(1):
-# i = 0
-# batch_list = process_data(data_1, i, i + 1, label_1)
-# for _ in range(len(batch_list)):  # range(len(w))
-#     K.set_value(a, batch_list[_][0][2][0])
-#     K.set_value(b, batch_list[_][0][2][1])
-#     # print(a, b)
-#     print('currently fitting {} batch for index {},{} and slice index are {},{}'.format(_,i,i+1,a,b))
-#     x = np.array([pad_cnn(batch_list[0][i][0][0]) for i in range(len(batch_list[_]))])
-#     x = x.reshape(x.shape[0], x.shape[1], x.shape[2], 1)
-#     lab = np.vstack([preprocess_single_label(batch_list[0][i][1])[1] for i in range(len(batch_list[_]))])
-#     model.fit(x, lab, batch_size=4)
-#############################################################################################################333
-#
-# t1 = time.time()
-# for _ in range(5):
-#     for q in range(700, data_1.shape[0]):
-#         if q % 100 == 0:
-#             print("CURRENTLY ON {}TH UTTERANCE OUT OF {},EPOCH:{}".format(q, data_1.shape[0], _))
-#             print("TOTAL TIME ELAPSED {} sec".format(time.time() - t1))
-#         if q % 1000 == 0:
-#             model.save("2d_cnn_model_dot_save")
-#             model.save_weights("2d_cnn_model_dot_save_weights_{}_utt".format(q))
-#         label_len = data_1[q][1].shape[0]
-#         x_train = pad_cnn(data_1[q][0])
-#         x_ = np.repeat(x_train.reshape(1, 1350, 40), label_len, axis=0)
-#         y_ = preprocess_single_label(label_1[q])  # 57,46
-#         for i, j, p in zip(x_, y_, time_boundary_pair(data_1[q])):
-#             #     print(i.shape)
-#             K.set_value(a, p[0])
-#             K.set_value(b, p[1])
-#             #             print(p)
-#             model.fit(x=i.reshape(1, 1350, 40, 1), y=j.reshape(1, 46), verbose=0)
+        with open("train_log_2d_cnn.txt","a") as file:
+            file.write("trained on index {},using {} utterances.total time elapsed : {} sec,accuracy is {}+\n".format(i,q,time.time()-t,accuracy))    
+    model.save('1d_CNN_dot_save_')
+    model.save_weights('1d_CNN_dot_weights_{}'.format(i))
+
+
 ##########################################################################
 #### PREDICTION PART  ###################################################
-#     all_pred=[]
-#     for q in range(dev_data.shape[0]):
-#         pred_list=[]
-#         label_len=dev_data[q][1].shape[0]
-#         x_dev=pad_cnn(dev_data[q][0])
-#         x_=np.repeat(x_dev.reshape(1,1350,40),label_len,axis=0)
-#         y_=preprocess_single_label(dev_labels[q])  # 57,46
-#         for i,j,p in zip(x_,y_,time_boundary_pair(dev_data[q])):
-#             K.set_value(a,p[0])
-#             K.set_value(b,p[1])
-#     #         print(p)
-#             pred_list.append(model.predict(x=i.reshape(1,2732,40)))
-#         all_pred.append(pred_list)
-
-#     acc=[]
-#     for i in range(len(all_pred)):
-#         p=np.argmax(np.vstack(all_pred[i]),axis=1)
-#         t=dev_labels[i]
-#         if p.shape == t.shape:
-#             acc.append(np.sum(np.equal(p,t))/np.equal(p,t).shape[0])
-#     print("accuracy is {}".format(np.array(acc).mean()))
-
-#     model.save("1d_cnn_model_dot_save")
-#     model.save_weights("1d_cnn_model_dot_save_weights_{}_acc".format(np.array(acc).mean()))
-
-
-# In[ ]:
-
-
-# model.save_weights("2d_cnn_model_dot_save_weights_5700_utt")
-
-# In[ ]:
-
 #
+# dev_data = np.load('11785-hw2pt2/dev-features.npy', encoding='bytes')
+# dev_labels = np.load('11785-hw2pt2/dev-labels.npy', encoding='bytes')
+
+
 # dev_data_1 = np.array([dev_data[i] for i in range(dev_data.shape[0]) if dev_data[i][0].shape[0] <= 1350])
 # dev_labels_1 = np.array([dev_labels[i] for i in range(dev_data.shape[0]) if dev_data[i][0].shape[0] <= 1350])
 
-# In[ ]:
 
-
-# dev_data_1.shape, dev_labels_1.shape
-
-# In[ ]:
 
 
 # all_pred = []
@@ -268,20 +253,7 @@ for i in range(1,800):
 #         pred_list.append(model.predict(x=i.reshape(1, 1350, 40, 1)))
 #     all_pred.append(pred_list)
 
-# In[ ]:
 
-
-# np.argmax(pred)
-
-
-# In[ ]:
-
-
-# len(all_pred)
-
-# In[ ]:
-
-#
 # acc = []
 # for i in range(len(all_pred)):
 #     p = np.argmax(np.vstack(all_pred[i]), axis=1)
